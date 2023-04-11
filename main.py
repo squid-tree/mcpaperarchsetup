04f4eb972d29b6286c93ffbe83dc3c6ef84cbb6d
squid-tree <acuriousquid@gmail.com>
2023-04-11 17:04:53 +1000

    edit

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ print("This script will set up your minecraft server")
 print("It will set up ufw (firewall), ssh for the current user, autologin, any dependencies and install the server")
 print("\n")
 
-import depends from modules
-import ufwsetup from modules
-import getty from modules
-import sshsetup from modules
-import installer from modules 
-import saveinstall from modules
+from modules import depends
+from modules import ufwsetup
+from modules import getty
+from modules import sshsetup
+from modules import installer
+from modules import saveinstall
