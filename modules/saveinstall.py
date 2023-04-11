b9e88ad57505790d797085453f06efaf7cfc5f89
squid-tree <acuriousquid@gmail.com>
2023-04-11 17:12:27 +1000

    edit

--- modules/saveinstall.py
+++ modules/saveinstall.py
@@ -1,3 +1,3 @@
 import os 
 from modules.scripts.config import *
-os.system("git clone https://github.com/squid-tree/mcserverinstaller.git %s" % homedirectory)
+os.system("git clone https://github.com/squid-tree/mcserverinstaller.git %s/mcserverinstaller" % homedirectory)
