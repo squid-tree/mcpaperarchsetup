623061223fa2ce2a713176eb6dd35f2f658791af
squid-tree <acuriousquid@gmail.com>
2023-04-11 22:42:50 +1000

    init

--- modules/mcserverbackup.py
+++ modules/mcserverbackup.py
@@ -6,13 +6,13 @@ print("Installing Mcserverbackup")
 print("Creating backup script... ")
 os.system('sudo mkdir /usr/share/mcbackupsoftware')
 os.system('sudo touch /usr/share/mcbackupsoftware/backup.py')
-os.system('sudo sh -c \'echo \"%s\" >> /usr/share/mcbackupsoftware/backup.py\'' % storedbackup)
+os.system('sudo sh -c \'printf \"%%s\" \"%s\" >> /usr/share/mcbackupsoftware/backup.py\'' % storedbackup)
 
 print("Setting script up to run daily ...")
 backupbscr="""#!/bin/bash
 /usr/bin/python3 /usr/share/mcbackupsoftware/backup.py"""
 os.system('sudo touch /usr/share/mcbackupsoftware/mcserverbackup.sh')
-os.system(str('sudo bash -c \'echo \"%s\" >> /usr/share/mcbackupsoftware/mcserverbackup.sh\'' % backupbscr))
+os.system(str('sudo bash -c \'printf \"%%s\" \"%s\" >> /usr/share/mcbackupsoftware/mcserverbackup.sh\'' % backupbscr))
 os.system(str('sudo ln -sf /usr/share/mcbackupsoftware/mcserverbackup.sh /etc/cron.daily/mcserverbackup.sh'))
 
 print("Setting up backup script binary at /usr/bin/mcserverbackup ...")
