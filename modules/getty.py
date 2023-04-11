9129a7e15039990d12f06e9ff71c7f43c9276b96
squid-tree <acuriousquid@gmail.com>
2023-04-11 17:09:54 +1000

    edit

--- modules/getty.py
+++ modules/getty.py
@@ -4,7 +4,8 @@ print("Setting up getty autologin")
 
 configstring=str("""[Service]
 ExecStart=
-ExecStart=-/sbin/agetty -o '-p -f -- "\\"u' --noclear --autologin %s %I $TERM""" % user)
+ExecStart=-/sbin/agetty -o '-p -f -- "\\"u' --noclear --autologin %s %%I $TERM""" % user)
+
 
 os.system("sudo -C \'echo %s >> /etc/systemd/system/getty@tty1.service.d/autologin.conf\'")
 
