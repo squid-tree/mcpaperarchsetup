596e1032d7645393dbdecd32935e29a30fd1c63a
squid-tree <acuriousquid@gmail.com>
2023-04-12 12:15:41 +1000

    fixedbackupscript

--- modules/getty.py
+++ modules/getty.py
@@ -1,12 +1,25 @@
 from modules.scripts.config import *
 import os
+
+def literaljoiner(l):
+    string=str()
+    for i in range(len(l)):
+        string += r'{}'.format(str("%s" % l[i]).replace('\n', '\\n'))
+
 print("Setting up getty autologin")
 
-configstring=str("""[Service]
-ExecStart=
-ExecStart=-/sbin/agetty -o '-p -f -- "\\"u' --noclear --autologin %s %%I $TERM""" % user)
+
+configstring=list([r"[Service]", "\n", r"ExecStart=", "\n", r"ExecStart=-/sbin/agetty -o '-p -f -- ", r"\\\\\\\\u", r"' ", r"--noclear --autologin ", r'{}'.format(str("%s" % user)), " %%I \$TERM"])
 
 
-os.system("sudo -C \'echo %s >> /etc/systemd/system/getty@tty1.service.d/autologin.conf\'")
+configstring = literaljoiner(configstring)
+
+print("Setting up getty directory ... ")
+
+os.system(str('sudo mkdir -p /etc/systemd/system/getty@tty.service.d'))
+
+print("Installing getty script")
+
+os.system(str('sudo bash -c \"printf \"%s\" > %s/penis.txt\"' % (configstring, str('/etc/systemd/system/getty@tty1.service.d/autologin.conf'))))
 
 print("Getty login has been setup")
