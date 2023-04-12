f5036c66b74b183e3737444311fd9d49a470f2bf
squid-tree <acuriousquid@gmail.com>
2023-04-12 17:54:25 +1000

    fkdls

--- modules/scripts/build.py
+++ modules/scripts/build.py
@@ -71,17 +71,11 @@ else:
 #    projectmodules.cleanupbuild()
 
 if projectmodules.confirminput('Wget this jar link? (Y/n): %s ' % jarlink):
-    if projectmodules.confirminput('Make wget directory?'):
-        print('%s' % homedirectory)
-        print(str('%s/mcserver/paperdirectory' % homedirectory))
-        os.system(str('mkdir -p %s/mcserver/paperdirectory' % homedirectory))
-        os.system("ls /home/rosa | grep mc")
-    else:
-        quit()
-    if projectmodules.confirminput('Do the wget?'):
-        os.system(str('wget %s -P %s' % (jarlink, str('%s/mcserver/paperdirectory' % homedirectory))))
-    else:
-        quit()
+    print('%s' % homedirectory)
+    print(str('%s/mcserver/paperdirectory' % homedirectory))
+    os.system(str('mkdir -p %s/mcserver/paperdirectory' % homedirectory))
+    os.system("ls /home/rosa | grep mc")
+    os.system(str('wget %s -P %s' % (jarlink, str('%s/mcserver/paperdirectory' % homedirectory))))
     print("Link was wgeted")
 else:
     projectmodules.cleanupbuild()
