5c0db0654d305530d7584cfb5df2210bcb3aa5d1
squid-tree <acuriousquid@gmail.com>
2023-04-12 14:09:44 +1000

    a

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import getpass
 userc = list(getpass.getuser()).copy()
 user = ''.join(userc)
+del getpass
+
+import os 
+scrdirectoryc = list(os.path.dirname(os.path.realpath(__file__))).copy()
+scrdirectory = ''.join(scrdirectoryc)
+del os
 
 x=input("Installing or uninstalling? (i/u)")
 if x == "i":
-    import superinstaller
+    exec(str('%s/superinstaller.py' % scrdirectory))
 elif x == "u":
-    import uninstaller
+    exec(str('%s/uninstaller.py' % scrdirectory))
 else:
     print("Invalid option")
     quit()
