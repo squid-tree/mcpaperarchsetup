24773b7222b048ef2756b76ef006771444d990fe
squid-tree <acuriousquid@gmail.com>
2023-04-12 18:01:02 +1000

    ksdlf

--- modules/scripts/uninstall.py
+++ modules/scripts/uninstall.py
@@ -9,7 +9,7 @@ if not projectmodules.confirminput("Are you sure you want to uninstall the serve
 print("Backup will be saved to %s/backupsmc" % homedirc)
 print("Saving Backup ...")
 os.system(str("mkdir %s/backupsmc" % homedirectory))
-os.system(str("sudo cp /opt/minecraft/mcserver %s/backupsmc" % homedirc))
+os.system(str("sudo cp -r /opt/minecraft/mcserver %s/backupsmc" % homedirc))
 print("Backup saved")
 print("Uninstalling ...")
 projectmodules.cleanupinstall()
