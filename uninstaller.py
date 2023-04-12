bda23ac000c98658f901015fb0dfe2ee37d3a7e3
squid-tree <acuriousquid@gmail.com>
2023-04-12 13:23:58 +1000

    a

--- uninstaller.py
+++ uninstaller.py
@@ -15,9 +15,12 @@ homedircopy = list(homedirectory)
 homedircopy = homedircopy.copy()
 homedircopy = ''.join(homedircopy)
 
+
+print("homedircopy", homedircopy)
+
 # mcserverbackup
 print("Uninstalling backup software and saving backups in home")
-os.system(str('sudo cp /usr/share/mcbackupssoftware/backups %s/' % homedircopy))
+os.system(str('sudo cp /usr/share/mcbackupsoftware/backups %s/' % homedircopy))
 os.system('sudo rm -rf /usr/bin/mcserverbackup')
 os.system('sudo rm -rf /etc/cron.daily/mcserverbackup.sh')
 os.system('sudo rm -rf /usr/mcbackupsoftware')
