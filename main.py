3d04b96db4c460d9fcfceccfa973f9dec69af5ff
squid-tree <acuriousquid@gmail.com>
2023-04-12 13:48:57 +1000

    addedmaster

--- main.py
+++ main.py
@@ -1,31 +1,13 @@
-# Setup ufw
-    # Enable ports 25565 and 22
-# Setup Ssh with {user} 
-# Set Getty Up
-# Install Dependencies
-# Install server 
 import getpass
-
+import modules.scripts.projectmodules
 userc = getpass.getuser().list().copy()
 user = ''.join(userc)
-print("Welcome")
-print("This script will set up your minecraft server")
-print("It will set up ufw (firewall), ssh for the current user, autologin, any dependencies, backup software and install the server")
-print("\n")
 
-if os.geteuid() == 0:
-    print("ERROR: Cannot be run as root")
+x=input("Installing or uninstalling? (i/u)")
+if x = "i"
+    import superinstaller
+elif x = "u"
+    import uninstaller
+else:
+    print("Invalid option")
     quit()
-
-if input("HAVE YOU FILLED OUT THE CONFIG (y/n)") != 'y':
-    quit()
-if input("DOUBLE CONFIRM, type fully (yes/no)") != 'yes':
-    quit()
-
-from modules import depends
-from modules import ufw
-from modules import getty
-from modules import sshsetup
-from modules import installer
-from modules import saveinstall
-from modules import mcserverbackup
