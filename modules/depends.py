6effbaf0ee6a77cf7d1057d0c246900b1eee521b
squid-tree <acuriousquid@gmail.com>
2023-04-13 21:48:56 +1000

    mksdlkf

--- modules/depends.py
+++ modules/depends.py
@@ -1,6 +1,6 @@
 import os
 print("Installing dependencies")
-os.system('sudo pacman -S openssh git github-cli ufw cronie wget')
+os.system('sudo pacman -S openssh git github-cli ufw cronie wget jdk-openjdk')
 held=os.popen('pacman -Qq').read()
 if 'yay' not in held:
     print("yay must be installed first")
