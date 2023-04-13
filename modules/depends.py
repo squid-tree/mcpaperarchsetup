840837b690249fd5a8abe64b4e245d604da4f73b
squid-tree <acuriousquid@gmail.com>
2023-04-13 13:11:51 +1000

    mfsdkl

--- modules/depends.py
+++ modules/depends.py
@@ -1,4 +1,8 @@
 import os
 print("Installing dependencies")
 os.system('sudo pacman -S openssh git github-cli ufw cronie wget yay')
+held=os.system('pacman -Qq')
+if 'yay' not in held:
+    print("yay must be installed first")
+    quit()
 os.system('yay -S mcrcon')
