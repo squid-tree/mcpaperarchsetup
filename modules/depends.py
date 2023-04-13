d831fb5abcd6d1149ea6594136541c74da550369
squid-tree <acuriousquid@gmail.com>
2023-04-13 13:08:48 +1000

    addedmcrcondep

--- modules/depends.py
+++ modules/depends.py
@@ -1,3 +1,4 @@
 import os
 print("Installing dependencies")
-os.system('sudo pacman -S openssh git github-cli ufw cronie wget')
+os.system('sudo pacman -S openssh git github-cli ufw cronie wget yay')
+os.system('yay -S mcrcon')
