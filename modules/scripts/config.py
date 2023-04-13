3b8aa01979f5ec1c0e93a54bc85934e1705228cf
squid-tree <acuriousquid@gmail.com>
2023-04-13 13:13:45 +1000

    sdmklf

--- modules/scripts/config.py
+++ modules/scripts/config.py
@@ -17,6 +17,7 @@ scrdirectory = ''.join(scrdirectoryc)
 cuser = list(open(str('%s/user.txt' % scrdirectory)).read()).copy()
 user = ''.join(cuser).strip()
 homedirectory = str('/home/%s' % user)
-rcpassword = open(str('%s/rcpassword.txt' % scrdirectory)).read().strip()
+if os.path.exists(str('%s/rcpassword.txt' % scrdirectory))
+    rcpassword = open(str('%s/rcpassword.txt' % scrdirectory)).read().strip()
 mcserviceconfig = str(serviceinfo % (ram, ramtype, ram, ramtype, jarversion, rcpassword))
 ##
