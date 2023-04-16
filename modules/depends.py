572fe37be8836fe7d65dd9f9ce97727ad5612770
squid-tree <acuriousquid@gmail.com>
2023-04-16 14:49:28 +1000

    kmldf

--- modules/depends.py
+++ modules/depends.py
@@ -1,8 +1,21 @@
 import os
 print("Installing dependencies")
+depends = ['openssh','git','github-cli','ufw','cronie','jdk-openjdk']
+for i in depends:
+    if i not in os.popen("pacman -Qq").read():
+        os.system(str('pacman -S %s' % i)
+    else:
+        print(str('Dependency %s is installed, skipping install' % i))
 os.system('sudo pacman -S openssh git github-cli ufw cronie wget jdk-openjdk')
 held=os.popen('pacman -Qq').read()
 if 'yay' not in held:
     print("yay must be installed first")
     quit()
-os.system('yay -S mcrcon')
+yaydeps=["mcrcon"]
+
+print("Yay is installed, checking yay dependencies")
+for i in yaydeps:
+    if i not in os.popen('pacman -Qq').read():
+        os.system(str('yay -S %s') % i)
+    else:
+        print(str('Dependency %s is installed, skipping install' % i))
