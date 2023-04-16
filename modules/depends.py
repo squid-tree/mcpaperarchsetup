d5336a0db63687a983ace59a096d3a579aec3527
squid-tree <acuriousquid@gmail.com>
2023-04-16 14:51:34 +1000

    mklds

--- modules/depends.py
+++ modules/depends.py
@@ -1,12 +1,11 @@
 import os
 print("Installing dependencies")
-depends = ['openssh','git','github-cli','ufw','cronie','jdk-openjdk']
+depends = ['openssh','git','github-cli','ufw','cronie','wget','jdk-openjdk']
 for i in depends:
     if i not in os.popen("pacman -Qq").read():
         os.system(str('pacman -S %s' % i))
     else:
         print(str('Dependency %s is installed, skipping install' % i))
-os.system('sudo pacman -S openssh git github-cli ufw cronie wget jdk-openjdk')
 held=os.popen('pacman -Qq').read()
 if 'yay' not in held:
     print("yay must be installed first")
