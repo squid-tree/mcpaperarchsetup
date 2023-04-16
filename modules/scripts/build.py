382485510e096a55d35fb3483e19b838f3d3d118
squid-tree <acuriousquid@gmail.com>
2023-04-16 14:54:46 +1000

    mkl

--- modules/scripts/build.py
+++ modules/scripts/build.py
@@ -24,13 +24,13 @@ if False in projectmodules.configchecker():
 else:
     print("Configs seem to be in place! (Again, double check)")
 
-print("Checking dependencies")
-packages = str(subprocess.check_output(['pacman', '-Qq']))
+#print("Checking dependencies")
+#packages = str(subprocess.check_output(['pacman', '-Qq']))
 
-if not projectmodules.isin(packages,dependencies):
-    print("There is a dependancy missing, do you have the following dependencies?")
-    print(dependencies)
-    quit()
+#if not projectmodules.isin(packages,dependencies):
+#    print("There is a dependancy missing, do you have the following dependencies?")
+#    print(dependencies)
+#    quit()
 else:
     print('All dependencies seem to be installed, make sure by checking below')
     print(dependencies)
@@ -49,80 +49,80 @@ else:
 #    print("Quit program")
 #    quit()
 
-if not projectmodules.confirminput("Are you sure you wish to continue?"):
-    print("Quit program")
-    quit()
+#if not projectmodules.confirminput("Are you sure you wish to continue?"):
+#    print("Quit program")
+#    quit()
 
 # Starting Stage
 print('\n')
 print("------Building------")
-if not projectmodules.confirminput("This is now the building stage, where the files will be downloaded in the home directory. Would you like to continue?"):
-    print("Quitting, only completed verification stage, no changes were made")
+#if not projectmodules.confirminput("This is now the building stage, where the files will be downloaded in the home directory. Would you like to continue?"):
+#print("Quitting, only completed verification stage, no changes were made")
 
-if projectmodules.confirminput("Create a new user named minecraft?"):
-    print("Adding user: minecraft")
-    os.system('sudo groupadd minecraft')
-    os.system('sudo useradd -r -m -d /opt/minecraft -s /bin/bash -g minecraft minecraft')
-else:
-    projectmodules.cleanupbuild()
-    projectmodules.cleanupinstall()
+#if projectmodules.confirminput("Create a new user named minecraft?"):
+print("Adding user: minecraft")
+os.system('sudo groupadd minecraft')
+os.system('sudo useradd -r -m -d /opt/minecraft -s /bin/bash -g minecraft minecraft')
+#else:
+ #   projectmodules.cleanupbuild()
+  #  projectmodules.cleanupinstall()
 
 #if projectmodules.confirminput("Clone the git repo in home?"):
 #    os.system('git clone %s %s' % (ghserverurl, str('%s/mcserver' % homedirectory)))
 #else:
 #    projectmodules.cleanupbuild()
 
-if projectmodules.confirminput('Wget this jar link? (Y/n): %s ' % jarlink):
-    print('%s' % homedirectory)
-    print(str('%s/mcserver/paperdirectory' % homedirectory))
-    os.system(str('mkdir -p %s/mcserver/paperdirectory' % homedirectory))
-    os.system("ls /home/rosa | grep mc")
-    os.system(str('wget %s -P %s' % (jarlink, str('%s/mcserver/paperdirectory' % homedirectory))))
-    print("Link was wgeted")
-else:
-    projectmodules.cleanupbuild()
-    projectmodules.cleanupinstall()
+#if projectmodules.confirminput('Wget this jar link? (Y/n): %s ' % jarlink):
+print('%s' % homedirectory)
+print(str('%s/mcserver/paperdirectory' % homedirectory))
+os.system(str('mkdir -p %s/mcserver/paperdirectory' % homedirectory))
+os.system("ls /home/rosa | grep mc")
+os.system(str('wget %s -P %s' % (jarlink, str('%s/mcserver/paperdirectory' % homedirectory))))
+print("Link was wgeted")
+#else:
+ #   projectmodules.cleanupbuild()
+  #  projectmodules.cleanupinstall()
 
 print("\n")
 print("------Installation------")    
-if not projectmodules.confirminput("This is now the installation stage, where the server will be installed into the system. Continue?"):
-    projectmodules.cleanupbuild()
-    projectmodules.cleanupinstall()
+#if not projectmodules.confirminput("This is now the installation stage, where the server will be installed into the system. Continue?"):
+    #projectmodules.cleanupbuild()
+    #projectmodules.cleanupinstall()
 
-if projectmodules.confirminput("Install mcserver to it's directory?"):
-    print("Installing mcserver ...")
-    projectmodules.os.system(str('sudo cp -r %s/mcserver /opt/minecraft/' % homedirectory))
-else:
-    projectmodules.cleanupbuild()
-    projectmodules.cleanupinstall()
+#if projectmodules.confirminput("Install mcserver to it's directory?"):
+print("Installing mcserver ...")
+projectmodules.os.system(str('sudo cp -r %s/mcserver /opt/minecraft/' % homedirectory))
+#else:
+#    projectmodules.cleanupbuild()
+#    projectmodules.cleanupinstall()
 
-if projectmodules.confirminput("Install the startmc service?"):
-    os.system('sudo touch /etc/systemd/system/startmc.service')
-    os.system('sudo sh -c \'echo \"%s\" >> /etc/systemd/system/startmc.service\'' % mcserviceconfig) 
-    print("Enabling groups access ...")
-    os.system('sudo chgrp -R minecraft /opt/minecraft/mcserver')
-    os.system('sudo chgrp -R minecraft /opt/minecraft/mcserver/paperdirectory')
-    os.system('sudo chgrp -R minecraft /opt/minecraft/mcserver/paperdirectory/\'%s\'' % jarversion)
-    
-    os.system('sudo chmod -R g+rwx /opt/minecraft')
-    os.system('sudo chmod -R g+rwx /opt/minecraft/mcserver')
-    os.system('sudo chmod -R g+rwx /opt/minecraft/mcserver/paperdirectory')
-    os.system('sudo chmod -R g+rwx /opt/minecraft/mcserver/paperdirectory/\'%s\'' % jarversion)
-    print("Giving java access ...")
-    os.system('sudo usermod -a -G minecraft jvmapps')
-else:
-    projectmodules.cleanupinstall()
-    projectmodules.cleanupbuild()
+#if projectmodules.confirminput("Install the startmc service?"):
+os.system('sudo touch /etc/systemd/system/startmc.service')
+os.system('sudo sh -c \'echo \"%s\" >> /etc/systemd/system/startmc.service\'' % mcserviceconfig) 
+print("Enabling groups access ...")
+os.system('sudo chgrp -R minecraft /opt/minecraft/mcserver')
+os.system('sudo chgrp -R minecraft /opt/minecraft/mcserver/paperdirectory')
+os.system('sudo chgrp -R minecraft /opt/minecraft/mcserver/paperdirectory/\'%s\'' % jarversion)
 
-if projectmodules.confirminput("Enable startmc service and it's java component?"):
-    print("Enabling startmc.service ...")
-    os.system('sudo systemctl daemon-reload')
-    os.system('sudo systemctl enable startmc.service')
-    os.system('sudo systemctl start startmc.service')
-    os.system('sudo systemctl stop startmc.service')
-else:
-    projectmodules.cleanupinstall()
-    projectmodules.cleanupbuild()
+os.system('sudo chmod -R g+rwx /opt/minecraft')
+os.system('sudo chmod -R g+rwx /opt/minecraft/mcserver')
+os.system('sudo chmod -R g+rwx /opt/minecraft/mcserver/paperdirectory')
+os.system('sudo chmod -R g+rwx /opt/minecraft/mcserver/paperdirectory/\'%s\'' % jarversion)
+print("Giving java access ...")
+os.system('sudo usermod -a -G minecraft jvmapps')
+#else:
+#    projectmodules.cleanupinstall()
+#    projectmodules.cleanupbuild()
+
+#if projectmodules.confirminput("Enable startmc service and it's java component?"):
+print("Enabling startmc.service ...")
+os.system('sudo systemctl daemon-reload')
+os.system('sudo systemctl enable startmc.service')
+os.system('sudo systemctl start startmc.service')
+os.system('sudo systemctl stop startmc.service')
+#else:
+ #   projectmodules.cleanupinstall()
+  #  projectmodules.cleanupbuild()
 
 print("Removing built mcserver directory at home")
 os.system(str('sudo rm -rf %s/mcserver' % homedirectory))
