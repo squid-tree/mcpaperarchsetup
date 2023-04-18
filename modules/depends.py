e620570e90ad63ce85efdcf19e5443d43acf39d5
squid-tree <acuriousquid@gmail.com>
2023-04-18 22:16:31 +1000

    fixeddepperms

--- modules/depends.py
+++ modules/depends.py
@@ -3,7 +3,7 @@ print("Installing dependencies")
 depends = ['openssh','git','github-cli','ufw','cronie','wget','jdk-openjdk']
 for i in depends:
     if i not in os.popen("pacman -Qq").read():
-        os.system(str('pacman -S %s' % i))
+        os.system(str('sudo pacman -S %s' % i))
     else:
         print(str('Dependency %s is installed, skipping install' % i))
 held=os.popen('pacman -Qq').read()
